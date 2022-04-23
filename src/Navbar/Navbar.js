ac6354822a0d3da88fe07d49810a2b9bf548f22a
Aryan Gupta <202051038@iiitvadodara.ac.in>
2022-04-23 10:01:02 +0530

    complete navbar

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='w-full h-[3] bg-black text-white p-2 px-5 font-bold text-4xl'>TIC TAC TOE</div>
+  )
+}
+
+export default Navbar
